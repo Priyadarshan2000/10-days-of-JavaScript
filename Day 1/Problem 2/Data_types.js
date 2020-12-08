746c1df683f173de5afe7dd513dd35b5a5671b63
Priyadarshan Ghosh <62868878+Priyadarshan2000@users.noreply.github.com>
2020-12-08 14:12:45 +0530

    Completed the day 1 on 6/12/2020

--- Day 1/Problem 2/Data_types.js
+++ Day 1/Problem 2/Data_types.js
@@ -8,15 +8,15 @@ function performOperation(secondInteger, secondDecimal, secondString) {
     // Declare a variable named 'firstString' and initialize with the string "HackerRank".
     const firstString = 'HackerRank ';
     
-    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number type) on a new line.
     console.log(firstInteger + Number(secondInteger));
   
    
-    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number type) on a new line.
     console.log(firstDecimal + Number(secondDecimal));
     
     
-    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The variable 'firstString' must be printed first.
     
     console.log(firstString.concat(secondString));
     
